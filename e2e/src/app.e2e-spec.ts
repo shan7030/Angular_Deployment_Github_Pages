233dc74c10f42976a44873279a8fd8cca2dcb65a
Shantanu <shan7030666366@gmail.com>
2021-01-04 17:38:10 +0530

    :pencil2: Changed the app name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('gfg-example app is running!');
+    expect(page.getTitleText()).toEqual('angular-example app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gfg-example'`, () => {
+  it(`should have as title 'angular-example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gfg-example');
+    expect(app.title).toEqual('angular-example');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gfg-example app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-example app is running!');
   });
 });
